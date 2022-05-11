1790d8b7e6f5057224ead86cb32d0e0eb1ec0e6f
Roberto <roberto.p.gomes@hotmail.com>
2022-05-11 17:00:17 -0300

    Forecast controller

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,32 @@
-// import './util/module-alias'
-// import { Server } from '@overnightjs/core'
-// import express from 'express'
+import './util/module-alias'
+import { Server } from '@overnightjs/core'
+import express from 'express'
+import { ForecastController } from './controllers/forecast';
+import { Application } from 'express'
 
 
-// export class SetupServer extends Server {
-//   constructor(private port = 3000) {
-//     super()
-//   }
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super()
+  }
 
-//   private setupExpress(): void {
-//     this.app.use(express.json())
-//   }
-// }
+  public init(): void {
+    this.setupExpress()
+    this.setupControllers()
+  }
+
+  private setupExpress(): void {
+    this.app.use(express.json())
+  }
+
+  private setupControllers(): void {
+    const forecastController = new ForecastController()
+    this.addControllers([forecastController])
+  }
+
+  public getApp(): Application {
+    return this.app
+  }
+}
 
 
