204f1aa3825a153019c9932bee999c5ced1026f4
Roberto <roberto.p.gomes@hotmail.com>
2022-05-11 08:19:46 -0300

    Jest

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+// import './util/module-alias'
+// import { Server } from '@overnightjs/core'
+// import express from 'express'
+
+
+// export class SetupServer extends Server {
+//   constructor(private port = 3000) {
+//     super()
+//   }
+
+//   private setupExpress(): void {
+//     this.app.use(express.json())
+//   }
+// }
+
+
